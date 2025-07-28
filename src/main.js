5c513aed6219ef06c01ddaa44a582201e847c997
Sudarmanto <sdr871@gmail.com>
2025-07-28 15:18:42 +0700

    membuat pinia

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import './assets/main.css'
+// import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
@@ -8,6 +8,6 @@ import { createPinia } from 'pinia'
 const app = createApp(App)
 const pinia = createPinia()
 
-app.use(router)
+app.use(router).use(pinia)
 
 app.mount('#app')
---
+++ src/views/TodoView.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>ini halaman todo list</h1>
+
+    {{ todoStore.name }}<br></br>
+    {{ todoStore.doubleCount }}<br></br>
+    <button @click="todoStore.increment">Add</button>
+</template>
+
+<script>
+    import { useTodoStore } from '@/stores/todoStore';
+
+    export default {
+        setup() {
+            const todoStore = useTodoStore()
+
+            return { todoStore }
+        }
+    }
+</script>
