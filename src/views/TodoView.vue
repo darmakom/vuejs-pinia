eeae5c562b4d543163994d3683d7a66235f81ae2
Sudarmanto <sdr871@gmail.com>
2025-07-28 15:30:21 +0700

    menambah spasi

--- src/views/TodoView.vue
+++ src/views/TodoView.vue
@@ -1,8 +1,8 @@
 <template>
     <h1>ini halaman todo list</h1>
 
-    {{ todoStore.name }}<br></br>
-    {{ todoStore.doubleCount }}<br></br>
+    {{ todoStore.name }}<br><br>
+    {{ todoStore.doubleCount }}<br><br>
     <button @click="todoStore.increment">Add</button>
 </template>
 
