c2d262b7dd8cb7313df2f2849ad7322ab1dc17d9
Sudarmanto <sdr871@gmail.com>
2025-08-28 09:04:34 +0700

    Hapus data todolist

--- src/stores/todoStore.js
+++ src/stores/todoStore.js
@@ -3,10 +3,10 @@ import { defineStore } from 'pinia'
 export const useTodoStore = defineStore('todo', {
   state: () => ({ 
     todoList : [
-        {name: 'Belajar HTML', isDone: false},
-        {name: 'Belajar CSS', isDone: false},
-        {name: 'Belajar JS', isDone: false},
-        {name: 'Belajar PHP', isDone: false},
+        //{name: 'Belajar HTML', isDone: false},
+        //{name: 'Belajar CSS', isDone: false},
+        //{name: 'Belajar JS', isDone: false},
+        //{name: 'Belajar PHP', isDone: false},
     ]
    }),
   getters: {
